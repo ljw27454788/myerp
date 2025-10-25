d4ef48fd9695206aa5473f9dcb073b69b83829cc
ljw27454788 <jay27454788@gmail.com>
2025-10-25 18:03:45 +0800

    A

--- factory/models.py
+++ factory/models.py
@@ -10,15 +10,31 @@ User = get_user_model()
 # Create your models here.
 
 # 基础信息
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=50, unique=True, verbose_name="材料种类名称")
+
+    class Meta:
+        verbose_name = "产品种类"
+
+    def __str__(self):
+        return self.name
+
 
 # 产品
 class Product(models.Model):
+    UNIT_CHOICES = [
+        ('kg', '千克'),
+        ('pcs', '个'),
+    ]
+    
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     in_name = models.CharField(max_length=200, unique=True, verbose_name="内部名称")
     out_name = models.CharField(max_length=200, unique=True, verbose_name="对外名称")
     code = models.CharField(max_length=50, unique=True, null=True, blank=True, verbose_name="产品编码")
+    unit = models.CharField(max_length=10, choices=UNIT_CHOICES, default='pcs', verbose_name="单位")
     price = models.DecimalField(max_digits=10, decimal_places=5, null=True, blank=True, verbose_name="产品价格")
     cost = models.DecimalField(max_digits=10, decimal_places=5, null=True, blank=True, verbose_name="产品成本")
+    category = models.ForeignKey(ProductCategory, on_delete=models.SET_NULL, null=True, blank=True,verbose_name="产品种类")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="更新时间")
     
@@ -67,6 +83,15 @@ class Supplier(models.Model):
     def __str__(self):
         return self.name
 
+class MaterialCategory(models.Model):
+    name = models.CharField(max_length=50, unique=True, verbose_name="材料种类名称")
+
+    class Meta:
+        verbose_name = "原料种类"
+
+    def __str__(self):
+        return self.name
+
 # 原料
 class Material(models.Model):
     UNIT_CHOICES = [
@@ -81,6 +106,7 @@ class Material(models.Model):
     unit = models.CharField(max_length=10, choices=UNIT_CHOICES, default='pcs', verbose_name="单位")
     unit_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, verbose_name="单价")
     supplier = models.ForeignKey(Supplier, on_delete=models.PROTECT, verbose_name="供应商")
+    category = models.ForeignKey(MaterialCategory, on_delete=models.SET_NULL, null=True, blank=True,verbose_name="原料种类")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="更新时间")
     
@@ -96,8 +122,6 @@ class ProductMaterial(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name="产品")
     material = models.ForeignKey(Material, on_delete=models.CASCADE, verbose_name="原料")
     quantity = models.DecimalField(max_digits=10, decimal_places=6, verbose_name="用量")
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
-    updated_at = models.DateTimeField(auto_now=True, verbose_name="更新时间")
     
     class Meta:
         unique_together = ("product", "material")
